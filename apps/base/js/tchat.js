6c1eac4992557d5671b07e9e6ada57f5152f03f1
BruhP8 <luc.biohazard@gmail.com>
2020-01-07 13:07:22 +0100

    main UI skeleton

---
+++ apps/base/js/tchat.js
@@ -0,0 +1,123 @@
+class TchatModel extends Model {
+
+	constructor() {
+		super();
+	}
+
+	async initialize(mvc) {
+		super.initialize(mvc);
+
+	}
+}
+
+class TchatView extends View {
+
+	constructor() {
+		super();
+		this.table = null;
+	}
+
+	initialize(mvc) {
+        super.initialize(mvc);
+        this.stage.style.display ="flex";
+        this.stage.style.alignItems = "center";
+        this.stage.justifiyContent = "space-between";
+        this.mainDiv = document.createElement("div");
+        this.mainDiv.style.display = "flex";
+        this.mainDiv.style.flexDirection = "column";
+		this.mainDiv.style.justifyContent = "space-between";
+		this.mainDiv.style.height = "100%";
+		this.mainDiv.style.width = "100%";
+        this.stage.appendChild(this.mainDiv);
+        // header section above discussio
+        this.headDiv = document.createElement("div");
+        this.mainDiv.appendChild(this.headDiv);
+        this.headDiv.style.display = "flex";
+        this.headDiv.style.justifyContent = "space-between";
+        this.headDiv.style.aligneItems = "center";
+        this.headDiv.style.width ="100%";
+        this.headDiv.style.height = "15%";
+        this.headDiv.style.borderBottom ="thick solid #000000";
+        this.menuButton = document.createElement("button");
+        this.menuButton.innerHTML = "Menu";
+		this.menuButton.style.fontSize = "15px";
+        this.headDiv.appendChild(this.menuButton);
+        // profile display
+        this.profileDiv = document.createElement("h3");
+        this.profileDiv.style.display = "flex";
+        this.profileDiv.style.aligneItems = "center";
+        this.profileDiv.style.marginRight = "15%";
+        this.profileDiv.innerHTML = "correspondant_name"
+        this.headDiv.appendChild(this.profileDiv);
+        // conversation history
+        this.convDiv = document.createElement("div");
+        this.convDiv.style.height = "70%";
+        this.convDiv.style.width = "100%";
+        this.mainDiv.appendChild(this.convDiv);
+        // text input and send button
+        this.inputDiv = document.createElement("div");
+        this.mainDiv.appendChild(this.inputDiv);
+        this.inputDiv.style.display = "flex";
+        this.inputDiv.style.justifyContent = "space-between";
+        this.inputDiv.style.height = "8%";
+        this.inputDiv.style.borderTop ="thick solid #000000";
+        this.textInput = document.createElement("INPUT");
+        this.textInput.style.overflow = "true";
+        this.textInput.setAttribute("placeholder", "Type your message here !");
+        this.textInput.setAttribute("type", "text");
+        this.textInput.style.width = "85%";
+        this.inputDiv.appendChild(this.textInput);
+        this.sendBtn = document.createElement("button");
+        this.sendBtn.style.width = "15%";
+        this.inputDiv.appendChild(this.sendBtn);
+        this.sendBtn.innerHTML = "Send";
+	}
+
+	// activate UI
+	activate() {
+		super.activate();
+		this.addListeners(); // listen to events
+	}
+
+	// deactivate
+	deactivate() {
+		super.deactivate();
+		this.removeListeners();
+	}
+
+	addListeners() {
+        // menu button lsitener
+        this.menuHandler = e => this.menuClick(e);
+        this.menuButton.addEventListener("click", this.menuHandler);
+	}
+
+	removeListeners() {
+        this.menuButton.removeEventListener("click", this.menuHandler);
+	}
+    // event triggers
+    menuClick() {
+		this.mvc.controller.menuClicked();		// link to the menu part of the controller
+    }
+
+}
+
+class TchatController extends Controller {
+
+	constructor() {
+		super();
+	}
+
+	initialize(mvc) {
+		super.initialize(mvc);
+
+	}
+    /**
+     * @method menuClicked : destroys the current view and start the menu MVC
+     */
+	async menuClicked() {
+		this.mvc.view.destroy(); 						     // destroy current view
+		this.mvc.app.mvc = this.mvc.app.testMVC;		     // change current MVC to the target MVC : menu
+		this.mvc.app.testMVC.view.attach(document.body);     // attach view of menu MVC
+		this.mvc.app.testMVC.view.activate(); 			     // activate user interface of menu MVC
+    }
+}
